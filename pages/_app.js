3f450961e3685f96db6be8bd84667b0b6eca59eb
juan <juancraft12yt@gmail.com>
2021-10-13 23:03:14 -0300

    Update pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,17 @@ import Head from "components/Head";
 import { ChakraProvider } from "@chakra-ui/react";
 import { customTheme } from "/styles/global";
 import AppLayout from "components/Layout";
+import UserContextProvider from "context/user";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
   return (
     <>
       <ChakraProvider theme={customTheme}>
+        <UserContextProvider>
           <Head />
           <AppLayout>
             <Component {...pageProps} />
           </AppLayout>
+        </UserContextProvider>
       </ChakraProvider>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,38 @@
-export default function Home() {
-  return <p>HOLA</p>;
+import ProductsList from "components/Products/List";
+import Title from "components/Title";
+import { Flex, Text } from "@chakra-ui/react";
+import { fetchProductsList } from "services/api";
+
+export default function Home({ error, data }) {
+  return (
+    <>
+      <Title />
+      {data && <ProductsList products={data} />}
+      {error && (
+        <Flex flex="1" justifyContent="center" alignItems="center">
+          <Text color="red" fontWeight="bold" fontSize="20px">
+            {error}
+          </Text>
+        </Flex>
+      )}
+    </>
+  );
+}
+export async function getStaticProps() {
+  try {
+    const data = await fetchProductsList();
+    return {
+      props: {
+        data,
+      },
+      revalidate: 300,
+    };
+  } catch (err) {
+    return {
+      props: {
+        error: err.message,
+      },
+      revalidate: 10,
+    };
+  }
 }
